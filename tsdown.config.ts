d0a04478ecff280cf1bae06f8750f2c48222cad9
Jordan Coeyman <jcoeyman@cloudflare.com>
2026-04-20 15:09:33 -0400

    fix: tsdown copy dest should be dir

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
   minify: true,
   sourcemap: false,
   external: ['@chenglou/pretext'],
-  copy: [{ from: 'src/style.css', to: 'dist/style.css' }],
+  copy: [{ from: 'src/style.css', to: 'dist/' }],
 });
